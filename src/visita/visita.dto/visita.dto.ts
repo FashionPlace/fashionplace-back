305c6f63ca93c9090b8d7cb70e753e0350513142
Daniel Arango <d.arangoc2@uniandes.edu.co>
2024-11-03 17:54:51 -0500

    Remove unused imports vista.dto

--- src/visita/visita.dto/visita.dto.ts
+++ src/visita/visita.dto/visita.dto.ts
@@ -1,8 +1,6 @@
 /* eslint-disable prettier/prettier */
 import { IsDateString, IsNotEmpty } from "class-validator";
-import { CompradorDto } from "src/comprador/comprador.dto/comprador.dto";
 import { CompradorEntity } from "src/comprador/comprador.entity/comprador.entity";
-import { ProductoDto } from "src/producto/producto.dto/producto.dto";
 import { ProductoEntity } from "src/producto/producto.entity/producto.entity";
 
 export class VisitaDto {
